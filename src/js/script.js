7226f3bab9871552695bffc4868fef2b185d9782
ValeriiaZhubrieva <perchik010102@gmail.com>
2021-01-19 23:52:36 +0200

    animate-menu

--- src/js/script.js
+++ src/js/script.js
@@ -46,18 +46,4 @@ $(document).ready(function(){
     }
 
     validateForms('#form');
-
-    $('form').submit(function(e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "mailer/smart.php",
-            data: $(this).serialize()
-        }).done(function() {
-            $(this).find("input").val("");
-
-            $('form').trigger('reset');
-        });
-        return false;
-    });
 });
